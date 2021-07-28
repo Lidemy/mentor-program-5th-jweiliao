fe448be3ed1f723afee18b2d24e54e7ed2bde86d
Wade <aaa51782@gmail.com>
2021-07-28 12:15:39 +0800

    week11 fix

--- homeworks/week11/hw1/handle_add_comment.php
+++ homeworks/week11/hw1/handle_add_comment.php
@@ -9,8 +9,24 @@
   }
 
   $username = $_SESSION['username'];
-
   $content = $_POST['content'];
+
+  $sql = "SELECT user_group_id FROM `wei_users` WHERE username = ?";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param('s', $username);
+  $result = $stmt->execute();
+  if (!$result) {
+    die($conn->error);
+  }
+  $result = $stmt->get_result();
+  $row = $result->fetch_assoc();
+  $user_group_id = $row['user_group_id'];
+
+  if($user_group_id === 4) {
+    echo "您沒有操作權限";
+    exit;
+  }
+
   $sql = "INSERT INTO wei_comments(username, content) VALUES(?, ?)";
   $stmt = $conn->prepare($sql);
   $stmt->bind_param("ss", $username, $content);
--- homeworks/week11/hw1/handle_update_comment.php
+++ homeworks/week11/hw1/handle_update_comment.php
@@ -12,9 +12,27 @@
   $id = $_POST['id'];
   $content = $_POST['content'];
 
-  $sql = "UPDATE wei_comments SET content = ? WHERE id = ? AND username = ?";
+  $sql = "SELECT user_group_id FROM `wei_users` WHERE username = ?";
   $stmt = $conn->prepare($sql);
-  $stmt->bind_param('sis', $content, $id, $username);
+  $stmt->bind_param('s', $username);
+  $result = $stmt->execute();
+  if (!$result) {
+    die($conn->error);
+  }
+  $result = $stmt->get_result();
+  $row = $result->fetch_assoc();
+  $user_group_id = $row['user_group_id'];
+
+  if($user_group_id === 1) {
+    $sql = "UPDATE wei_comments SET content = ? WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('si', $content, $id);
+  } else {
+    $sql = "UPDATE wei_comments SET content = ? WHERE id = ? AND username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('sis', $content, $id, $username);
+  }
+
   $result = $stmt->execute();
   if (!$result) {
     die($conn->error);
